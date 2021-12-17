da0e14b3b564a83d8e9ea5a272ed35aaa6cf150e
UnleqitQ <quentin.weinbrenner@gmail.com>
2021-12-17 16:41:31 +0100

    Initial commit

---
+++ src/main/java/me/unleqitq/vanillaplustweaks/VanillaPlusTweaks.java
@@ -0,0 +1,18 @@
+package me.unleqitq.vanillaplustweaks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VanillaPlusTweaks extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+	
+}
