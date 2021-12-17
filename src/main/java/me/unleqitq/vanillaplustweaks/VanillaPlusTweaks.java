9181c8187fa1a081e731938d65581505c6b55eb6
UnleqitQ <quentin.weinbrenner@gmail.com>
2021-12-17 18:55:27 +0100

    update

--- src/main/java/me/unleqitq/vanillaplustweaks/VanillaPlusTweaks.java
+++ src/main/java/me/unleqitq/vanillaplustweaks/VanillaPlusTweaks.java
@@ -4,12 +4,16 @@ import me.unleqitq.vanillaplustweaks.listeners.FlintAndSteelListener;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Random;
+
 public final class VanillaPlusTweaks extends JavaPlugin {
 	
 	private static VanillaPlusTweaks instance;
+	private final Random random;
 	
 	public VanillaPlusTweaks() {
 		instance = this;
+		random = new Random();
 	}
 	
 	@Override
@@ -23,6 +27,10 @@ public final class VanillaPlusTweaks extends JavaPlugin {
 	
 	public static VanillaPlusTweaks getInstance() {
 		return instance;
+	}
+	
+	public Random getRandom() {
+		return random;
 	}
 	
 }
--- src/main/java/me/unleqitq/vanillaplustweaks/listeners/FlintAndSteelListener.java
+++ src/main/java/me/unleqitq/vanillaplustweaks/listeners/FlintAndSteelListener.java
@@ -3,6 +3,7 @@ package me.unleqitq.vanillaplustweaks.listeners;
 import me.unleqitq.vanillaplustweaks.VanillaPlusTweaks;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -13,11 +14,15 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 
+import java.util.Random;
+import java.util.Set;
+
 public class FlintAndSteelListener implements Listener {
 	
 	public FlintAndSteelListener() {
 		Bukkit.getPluginManager().registerEvents(this, VanillaPlusTweaks.getInstance());
 	}
+	
 	
 	@EventHandler
 	public void onHit(EntityDamageByEntityEvent event) {
@@ -25,25 +30,31 @@ public class FlintAndSteelListener implements Listener {
 			Player player = (Player) event.getDamager();
 			ItemStack item = player.getInventory().getItemInMainHand();
 			if (item.getType() == Material.FLINT_AND_STEEL) {
-				if (item.hasItemMeta() && item.getItemMeta() instanceof Damageable) {
-					Damageable meta = (Damageable) item.getItemMeta();
-					meta.setDamage(meta.getDamage() + 1);
-					item.setItemMeta(meta);
-				}
-				event.getEntity().setFireTicks(event.getEntity().getFireTicks() + 40);
-			}
-		}
-		if (event.getDamager() instanceof LivingEntity) {
-			LivingEntity damager = (LivingEntity) event.getDamager();
-			if (damager.getEquipment() != null) {
-				ItemStack item = damager.getEquipment().getItemInMainHand();
-				if (item.getType() == Material.FLINT_AND_STEEL) {
-					if (item.hasItemMeta() && item.getItemMeta() instanceof Damageable) {
+				if (item.getItemMeta() instanceof Damageable) {
+					int level = item.getEnchantmentLevel(Enchantment.DURABILITY);
+					if (VanillaPlusTweaks.getInstance().getRandom().nextInt(level + 1) == 0) {
 						Damageable meta = (Damageable) item.getItemMeta();
 						meta.setDamage(meta.getDamage() + 1);
 						item.setItemMeta(meta);
 					}
 					event.getEntity().setFireTicks(event.getEntity().getFireTicks() + 40);
+				}
+			}
+		}
+		else if (event.getDamager() instanceof LivingEntity) {
+			LivingEntity damager = (LivingEntity) event.getDamager();
+			if (damager.getEquipment() != null) {
+				ItemStack item = damager.getEquipment().getItemInMainHand();
+				if (item.getType() == Material.FLINT_AND_STEEL) {
+					if (item.getItemMeta() instanceof Damageable) {
+						int level = item.getEnchantmentLevel(Enchantment.DURABILITY);
+						if (VanillaPlusTweaks.getInstance().getRandom().nextInt(level + 1) == 0) {
+							Damageable meta = (Damageable) item.getItemMeta();
+							meta.setDamage(meta.getDamage() + 1);
+							item.setItemMeta(meta);
+						}
+						event.getEntity().setFireTicks(event.getEntity().getFireTicks() + 40);
+					}
 				}
 			}
 		}
