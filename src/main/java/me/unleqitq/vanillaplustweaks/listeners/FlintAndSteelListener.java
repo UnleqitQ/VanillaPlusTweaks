8dbf1671b822b68cbd2a0547ae0c08f9bd1e3b68
UnleqitQ <quentin.weinbrenner@gmail.com>
2021-12-17 17:49:11 +0100

    update

--- src/main/java/me/unleqitq/vanillaplustweaks/listeners/FlintAndSteelListener.java
+++ src/main/java/me/unleqitq/vanillaplustweaks/listeners/FlintAndSteelListener.java
@@ -2,12 +2,20 @@ package me.unleqitq.vanillaplustweaks.listeners;
 
 import me.unleqitq.vanillaplustweaks.VanillaPlusTweaks;
 import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class FlintAndSteelListener implements Listener {
 	
 	public FlintAndSteelListener() {
 		Bukkit.getPluginManager().registerEvents(this, VanillaPlusTweaks.getInstance());
+	}
+	
+	@EventHandler
+	public void onHit(PlayerInteractEvent event) {
+		event.getPlayer().sendMessage(event.getAction() + "", event.getEventName(), event.getBlockFace() + "",
+				event.getClickedBlock() + "", event.getHand() + "", event.getItem() + "", event.getMaterial() + "");
 	}
 	
 }
