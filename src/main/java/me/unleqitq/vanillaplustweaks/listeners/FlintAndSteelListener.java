58e303863267ba422f6fba0277fcd83b634d55d0
UnleqitQ <quentin.weinbrenner@gmail.com>
2021-12-17 18:19:52 +0100

    update

--- src/main/java/me/unleqitq/vanillaplustweaks/listeners/FlintAndSteelListener.java
+++ src/main/java/me/unleqitq/vanillaplustweaks/listeners/FlintAndSteelListener.java
@@ -2,9 +2,16 @@ package me.unleqitq.vanillaplustweaks.listeners;
 
 import me.unleqitq.vanillaplustweaks.VanillaPlusTweaks;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
 
 public class FlintAndSteelListener implements Listener {
 	
@@ -13,11 +20,33 @@ public class FlintAndSteelListener implements Listener {
 	}
 	
 	@EventHandler
-	public void onHit(PlayerInteractEvent event) {
-		event.getPlayer().sendMessage("Action:        " + event.getAction(), "EventName:     " + event.getEventName(),
-				"BlockFace:     " + event.getBlockFace(), "Clicked Block: " + event.getClickedBlock(),
-				"Hand:          " + event.getHand(), "Item:          " + event.getItem(),
-				"Material:      " + event.getMaterial(), "", "");
+	public void onHit(EntityDamageByEntityEvent event) {
+		if (event.getDamager() instanceof Player) {
+			Player player = (Player) event.getDamager();
+			ItemStack item = player.getInventory().getItemInMainHand();
+			if (item.getType() == Material.FLINT_AND_STEEL) {
+				if (item.hasItemMeta() && item.getItemMeta() instanceof Damageable) {
+					Damageable meta = (Damageable) item.getItemMeta();
+					meta.setDamage(meta.getDamage() + 1);
+					item.setItemMeta(meta);
+				}
+				event.getEntity().setFireTicks(event.getEntity().getFireTicks() + 40);
+			}
+		}
+		if (event.getDamager() instanceof LivingEntity) {
+			LivingEntity damager = (LivingEntity) event.getDamager();
+			if (damager.getEquipment() != null) {
+				ItemStack item = damager.getEquipment().getItemInMainHand();
+				if (item.getType() == Material.FLINT_AND_STEEL) {
+					if (item.hasItemMeta() && item.getItemMeta() instanceof Damageable) {
+						Damageable meta = (Damageable) item.getItemMeta();
+						meta.setDamage(meta.getDamage() + 1);
+						item.setItemMeta(meta);
+					}
+					event.getEntity().setFireTicks(event.getEntity().getFireTicks() + 40);
+				}
+			}
+		}
 	}
 	
 }
