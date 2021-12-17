576afe6d20e99c5edf8da6bf48f65730413a8236
UnleqitQ <quentin.weinbrenner@gmail.com>
2021-12-17 22:50:41 +0100

    update

--- src/main/java/me/unleqitq/vanillaplustweaks/Configuration.java
+++ src/main/java/me/unleqitq/vanillaplustweaks/Configuration.java
@@ -6,6 +6,8 @@ import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 
 public final class Configuration {
@@ -13,6 +15,20 @@ public final class Configuration {
 	private static File folder;
 	private static File configFile;
 	private static YamlConfiguration config;
+	
+	private static Map<String, Object> defaults = new HashMap<>();
+	
+	static {
+		createDefaults();
+	}
+	
+	private static void createDefaults() {
+		defaults.put("flintAndSteel.enable", true);
+		defaults.put("flintAndSteel.hitEntityIgnite.enable", true);
+		defaults.put("flintAndSteel.hitEntityIgnite.ticks", 40);
+		defaults.put("flintAndSteel.smeltBlocks.enable", true);
+		defaults.put("flintAndSteel.smeltBlocks.useFortune", true);
+	}
 	
 	private Configuration() {}
 	
@@ -29,13 +45,7 @@ public final class Configuration {
 		}
 		config = new YamlConfiguration();
 		if (configFile.exists()) {
-			try {
-				config.load(configFile);
-			} catch (IOException e) {
-				Bukkit.getLogger().log(Level.WARNING, "Could not access config", e);
-			} catch (InvalidConfigurationException e) {
-				Bukkit.getLogger().log(Level.WARNING, "Config is invalid", e);
-			}
+			config = YamlConfiguration.loadConfiguration(configFile);
 		}
 		else {
 			Bukkit.getLogger().log(Level.WARNING, "Config not found");
@@ -45,11 +55,46 @@ public final class Configuration {
 		config.addDefault("flintAndSteel.hitEntityIgnite.ticks", 40);
 		config.addDefault("flintAndSteel.smeltBlocks.enable", true);
 		config.addDefault("flintAndSteel.smeltBlocks.useFortune", true);
+		addDefaults();
 		if (configFile.exists()) {
 			try {
 				config.save(configFile);
 			} catch (IOException e) {
 				Bukkit.getLogger().log(Level.WARNING, "Could not save config", e);
+			}
+		}
+		config = YamlConfiguration.loadConfiguration(configFile);
+	}
+	
+	private static void addDefaults() {
+		for (Map.Entry<String, Object> entry : defaults.entrySet()) {
+			if (!config.contains(entry.getKey(), true)) {
+				config.set(entry.getKey(), entry.getValue());
+				continue;
+			}
+			if (entry.getValue() instanceof String) {
+				if (!config.isString(entry.getKey())) {
+					config.set(entry.getKey(), entry.getValue());
+				}
+				continue;
+			}
+			if (entry.getValue() instanceof Integer) {
+				if (!config.isInt(entry.getKey())) {
+					config.set(entry.getKey(), entry.getValue());
+				}
+				continue;
+			}
+			if (entry.getValue() instanceof Boolean) {
+				if (!config.isBoolean(entry.getKey())) {
+					config.set(entry.getKey(), entry.getValue());
+				}
+				continue;
+			}
+			if (entry.getValue() instanceof Double) {
+				if (!config.isDouble(entry.getKey())) {
+					config.set(entry.getKey(), entry.getValue());
+				}
+				continue;
 			}
 		}
 	}
